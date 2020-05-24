4275d0588db2a648983a3d0611fa90133b8c9967
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-05-24 06:25:10 +0000

    Configure Travis

--- app/src/androidTest/java/com/example/stockapp/Database/StockDatabaseTest.kt
+++ app/src/androidTest/java/com/example/stockapp/Database/StockDatabaseTest.kt
@@ -2,4 +2,5 @@ package com.example.stockapp.Database
 
 import androidx.room.Entity
 import androidx.room.Room
-import androidx.test.ext.junit.ru
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.
