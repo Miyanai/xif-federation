cfbb5f0902b48b5beadd2328727fc0af4515304a
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-07-05 11:20:22 +0000

    Update billy.md

--- app/src/main/java/com/example/stockapp/AppModule/AppModule.kt
+++ app/src/main/java/com/example/stockapp/AppModule/AppModule.kt
@@ -1,2 +1,22 @@
 
 package com.example.stockapp.AppModule
+
+import android.content.Context
+import com.example.stockapp.Application.ApplicationClass
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun toApp(@ApplicationContext app: Context):ApplicationClass{
+        return app as ApplicationClass
+    }
+}
---
+++ app/src/main/java/com/example/stockapp/AppModule/DatabaseModule.kt
@@ -0,0 +1,3 @@
+package com.example.stockapp.AppModule
+
+impo
