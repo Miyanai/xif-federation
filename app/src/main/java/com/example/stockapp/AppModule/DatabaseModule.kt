fbec28e4b16342974278de988d6e1028db55e2c8
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-07-07 07:58:49 +0000

    KISS

--- app/src/main/java/com/example/stockapp/AppModule/DatabaseModule.kt
+++ app/src/main/java/com/example/stockapp/AppModule/DatabaseModule.kt
@@ -4,4 +4,5 @@ import android.app.Application
 import androidx.room.Room
 import androidx.room.migration.Migration
 import com.example.stockapp.Database.MIGRATION_1_2
-import com.e
+import com.example.stockapp.Database.StockDatabase
+import dagge
