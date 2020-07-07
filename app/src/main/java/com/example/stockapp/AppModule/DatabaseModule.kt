c3a5d7f2b5f648f0410e3521df242260f2828332
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-07-07 07:57:13 +0000

    Remove garbage.

--- app/src/main/java/com/example/stockapp/AppModule/DatabaseModule.kt
+++ app/src/main/java/com/example/stockapp/AppModule/DatabaseModule.kt
@@ -3,4 +3,5 @@ package com.example.stockapp.AppModule
 import android.app.Application
 import androidx.room.Room
 import androidx.room.migration.Migration
-import com.example.
+import com.example.stockapp.Database.MIGRATION_1_2
+import com.e
