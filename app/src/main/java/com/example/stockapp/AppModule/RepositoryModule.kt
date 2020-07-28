10662b0d2e69ca7786fc7a2e0e9ed0305bce49b4
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-07-28 21:23:06 +0000

    updated tools

--- app/src/main/java/com/example/stockapp/AppModule/RepositoryModule.kt
+++ app/src/main/java/com/example/stockapp/AppModule/RepositoryModule.kt
@@ -2,4 +2,5 @@ package com.example.stockapp.AppModule
 
 import com.example.stockapp.Database.StockDao
 import com.example.stockapp.Database.StockDatabase
-import com.example.stockapp.Repository.Stock
+import com.example.stockapp.Repository.StockRepository
+import com.example.stockapp.Repository.St
