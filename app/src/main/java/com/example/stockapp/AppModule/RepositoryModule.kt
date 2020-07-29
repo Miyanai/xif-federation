341838b610d4112232ac2209774d243d5322c588
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-07-29 06:25:52 +0000

    codacy

--- app/src/main/java/com/example/stockapp/AppModule/RepositoryModule.kt
+++ app/src/main/java/com/example/stockapp/AppModule/RepositoryModule.kt
@@ -3,4 +3,6 @@ package com.example.stockapp.AppModule
 import com.example.stockapp.Database.StockDao
 import com.example.stockapp.Database.StockDatabase
 import com.example.stockapp.Repository.StockRepository
-import com.example.stockapp.Repository.St
+import com.example.stockapp.Repository.StockRepositoryList
+import dagger.Module
+import dagger.Provides
