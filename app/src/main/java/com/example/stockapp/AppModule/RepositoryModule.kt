789d3bf18a6d255162c5b2c43aea4bc23fb143f1
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-07-31 07:39:20 +0000

    msm: ADSPRPC: Initialize hlist node pointers

--- app/src/main/java/com/example/stockapp/AppModule/RepositoryModule.kt
+++ app/src/main/java/com/example/stockapp/AppModule/RepositoryModule.kt
@@ -8,4 +8,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::c
