9ef845c3386cd3fd2f27b51f59405f80c928c191
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-08-02 21:44:02 +0000

    add regression test

--- app/src/main/java/com/example/stockapp/BottomScreen.kt
+++ app/src/main/java/com/example/stockapp/BottomScreen.kt
@@ -2,4 +2,5 @@ package com.example.stockapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity
+import androidx.activity.compose.setContent
+import androidx.activity.vi
