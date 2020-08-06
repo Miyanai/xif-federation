6f26f37151739084ef61326791335483e9d7e860
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-08-06 18:13:01 +0000

    fixed read me

--- app/src/main/java/com/example/stockapp/BottomScreen.kt
+++ app/src/main/java/com/example/stockapp/BottomScreen.kt
@@ -13,4 +13,5 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compos
+import androidx.compose.ui.res.painterResource
+import androidx.compose.
