b82cfe06c14f437cd37134eb2e3ef8d0337946a2
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-08-06 21:01:42 +0000

    release v1.1.1

--- app/src/main/java/com/example/stockapp/BottomScreen.kt
+++ app/src/main/java/com/example/stockapp/BottomScreen.kt
@@ -14,4 +14,5 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose
