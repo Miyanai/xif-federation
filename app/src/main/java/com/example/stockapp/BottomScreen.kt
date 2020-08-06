3b0b97fb2541d95721406c530810568fe2b27fdd
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-08-06 15:06:58 +0000

    Some small fixes.

--- app/src/main/java/com/example/stockapp/BottomScreen.kt
+++ app/src/main/java/com/example/stockapp/BottomScreen.kt
@@ -12,4 +12,5 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.co
+import androidx.compose.ui.graphics.Color
+import androidx.compos
