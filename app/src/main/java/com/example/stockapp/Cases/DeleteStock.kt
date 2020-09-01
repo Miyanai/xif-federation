08767eecdbe13b7cbf691faa64c7bf0ff454755d
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-09-01 09:57:49 +0000

    Update _navigation.scss

--- app/src/main/java/com/example/stockapp/Cases/DeleteStock.kt
+++ app/src/main/java/com/example/stockapp/Cases/DeleteStock.kt
@@ -2,4 +2,7 @@ package com.example.stockapp.Cases
 
 import com.example.stockapp.Database.StockEntity
 import com.example.stockapp.Repository.StockRepository
-import javax.inject.I
+import javax.inject.Inject
+
+class DeleteStock @Inject constructor(
+    
