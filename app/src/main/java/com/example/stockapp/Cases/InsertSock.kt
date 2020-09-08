6a23d8babbabdf9fd1c123d70def8cce0788fca7
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-09-08 19:29:10 +0000

    spec updates

--- app/src/main/java/com/example/stockapp/Cases/InsertSock.kt
+++ app/src/main/java/com/example/stockapp/Cases/InsertSock.kt
@@ -1,4 +1,8 @@
 package com.example.stockapp.Cases
 
 import com.example.stockapp.Database.StockEntity
-import com.example.stockapp.Repository.Stock
+import com.example.stockapp.Repository.StockRepository
+import javax.inject.Inject
+
+
+class Inser
