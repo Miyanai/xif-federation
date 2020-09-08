5cd03c62f38cb4daada124177cc65b6118e60ac0
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-09-08 07:48:58 +0000

    Fixed small issues

--- app/src/main/java/com/example/stockapp/Cases/GetStock.kt
+++ app/src/main/java/com/example/stockapp/Cases/GetStock.kt
@@ -3,4 +3,6 @@ package com.example.stockapp.Cases
 import com.example.stockapp.Database.StockEntity
 import com.example.stockapp.Repository.StockRepository
 import kotlinx.coroutines.flow.Flow
-import jav
+import javax.inject.Inject
+
+class GetStock @Inject const
