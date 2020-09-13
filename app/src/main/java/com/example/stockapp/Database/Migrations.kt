9da3ba83bf8f46fe3402024229bc22ba9c540ecb
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-09-13 15:17:11 +0000

    Add release date

--- app/src/main/java/com/example/stockapp/Database/Migrations.kt
+++ app/src/main/java/com/example/stockapp/Database/Migrations.kt
@@ -6,3 +6,6 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 
 val MIGRATION_1_2 = object : Migration(1, 2){
     override fun migrate(database: SupportSQLiteDatabase) {
+      database.execSQL("ALTER TABLE `Stock` MODIFY COLUMN `stockLocation` TEXT, `stockName` TEXT, `barcode` TEXT)")
+    }
+}
---
+++ app/src/main/java/com/example/stockapp/Database/StockDao.kt
@@ -0,0 +1,22 @@
+
+package com.example.stockapp.Database
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface StockDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStock(stockEntity: StockEntity)
+
+    @Delete
+    suspend fun deleteStock(stockEntity: StockEntity)
+
+    @Update
+    suspend fun UpdateStock(stockEntity: StockEntity)
+
+    @Query("SELECT * FROM stock")
+    fun getStock(): List<StockEntity>
+
+}
