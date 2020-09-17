e4a0baf29b4a267c658c26489e615847280fdc0b
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-09-17 21:32:37 +0000

    bump version 0.0.3

--- app/src/main/java/com/example/stockapp/Database/StockDatabase.kt
+++ app/src/main/java/com/example/stockapp/Database/StockDatabase.kt
@@ -3,4 +3,6 @@ package com.example.stockapp.Database
 import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RenameTable
-impo
+import androidx.room.RoomDatabase
+
+@Database(entit
