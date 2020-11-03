bb8abe7e8a7f4210d3c77bb76f963c96d3b44ef6
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-11-03 18:11:42 +0000

    Updating Automatically

--- app/src/main/java/com/example/stockapp/Database/StockEvent.kt
+++ app/src/main/java/com/example/stockapp/Database/StockEvent.kt
@@ -2,4 +2,5 @@ package com.example.stockapp.Database
 
 sealed class StockEvent{
 
-data class DeleteStock (val stockEntity: StockEntity): StockE
+data class DeleteStock (val stockEntity: StockEntity): StockEvent()
+    data class InsertStock (val s
