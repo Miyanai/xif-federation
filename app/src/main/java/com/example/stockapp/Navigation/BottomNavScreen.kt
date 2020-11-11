c15ced115d2dbd731a93a9ea16f103c049857bb2
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-11-11 11:17:50 +0000

    Update configs.

--- app/src/main/java/com/example/stockapp/Navigation/BottomNavScreen.kt
+++ app/src/main/java/com/example/stockapp/Navigation/BottomNavScreen.kt
@@ -4,3 +4,54 @@ package com.example.stockapp.Navigation
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.stockapp.R
+
+@Composable
+fun BottomNavScreen (navController: NavController) {
+
+    BottomNavigation(
+        backgroundColor = colorResource(id = R.color.white)
+
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+
+        BottomNavigationItem(
+
+            selected = currentDestination?.route == "Stocks",
+            onClick = {navController.navigate("Stocks")},
+            label = { Text(text = "Stocks") },
+            icon = {
+                Icon(
+                    painterResource(R.drawable.ic_baseline_inventory_24),
+                    contentDescription = null,
+                    tint = Color.Magenta
+                )
+            },
+
+            )
+
+        BottomNavigationItem(selected = currentDestination?.route == "Scan/code" ,
+            onClick = { navController.navigate("Scan/code")},
+            label = { Text(text = "Scan") },
+            icon = { Icon(
+                painterResource( id = R.drawable.ic_baseline_qr_code_scanner_24),
+                contentDescription = null,
+                tint = Color.Magenta)
+
+            }
+
+
+        )
+        BottomNavigationItem(selected = currentDestination?.route  == "Reports" ,
+            onClick = { navController.navigate( "Reports")},
+            label = { Text(text = "Reports") },
+            icon = { Icon(
