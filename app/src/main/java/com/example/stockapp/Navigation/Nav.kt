3b0e369e0c85aeedf6b906e3961dfb3866bec268
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-11-29 19:53:06 +0000

    Version 0.0.10

--- app/src/main/java/com/example/stockapp/Navigation/Nav.kt
+++ app/src/main/java/com/example/stockapp/Navigation/Nav.kt
@@ -22,4 +22,5 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import andro
+import androidx.compose.ui.platform.LocalLifecycleOwner
+i
