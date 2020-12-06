d667155866f5c4ca0454341563c4d9536920d2c5
Miyanai <106360905+Miyanai@users.noreply.github.com>
2020-12-06 16:46:54 +0000

    Update about.html

--- app/src/main/java/com/example/stockapp/Navigation/Nav.kt
+++ app/src/main/java/com/example/stockapp/Navigation/Nav.kt
@@ -28,4 +28,5 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-impor
+import androidx.compose.ui.viewinterop.AndroidView
+import andr
