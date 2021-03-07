e52ce0bc0880d020aae15767591b0ff356e08b69
Miyanai <106360905+Miyanai@users.noreply.github.com>
2021-03-07 07:29:58 +0000

    Automated update repository metadata [skip ci]

--- app/src/main/java/com/example/stockapp/Recyclerview/StockItem.kt
+++ app/src/main/java/com/example/stockapp/Recyclerview/StockItem.kt
@@ -2,4 +2,6 @@ package com.example.stockapp.Recyclerview
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCo
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import android
