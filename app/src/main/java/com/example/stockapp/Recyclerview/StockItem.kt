1a7ab4c839f512bec139ced52df1bf1b89e93e18
Miyanai <106360905+Miyanai@users.noreply.github.com>
2021-03-14 09:42:40 +0000

    modified composer.json

--- app/src/main/java/com/example/stockapp/Recyclerview/StockItem.kt
+++ app/src/main/java/com/example/stockapp/Recyclerview/StockItem.kt
@@ -12,4 +12,5 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.t
