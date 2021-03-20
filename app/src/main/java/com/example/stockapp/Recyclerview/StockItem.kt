3e2d5416e8649683aecfc34240c9edf18c970bcb
Miyanai <106360905+Miyanai@users.noreply.github.com>
2021-03-20 13:36:28 +0000

    Improve UX

--- app/src/main/java/com/example/stockapp/Recyclerview/StockItem.kt
+++ app/src/main/java/com/example/stockapp/Recyclerview/StockItem.kt
@@ -15,4 +15,6 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+im
