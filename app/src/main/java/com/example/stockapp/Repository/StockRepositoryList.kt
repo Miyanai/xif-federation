9b331be18312c7baad6a211dc5faa7ca3b0774d1
Miyanai <106360905+Miyanai@users.noreply.github.com>
2021-05-16 17:30:16 +0000

    removed unused scripts

--- app/src/main/java/com/example/stockapp/Repository/StockRepositoryList.kt
+++ app/src/main/java/com/example/stockapp/Repository/StockRepositoryList.kt
@@ -5,4 +5,5 @@ import com.example.stockapp.Database.StockEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class StockRepositoryList @Inject constructor (private val stockDatabase: StockDatabase): StockRep
+class StockRepositoryList @Inject constructor (private val stockDatabase: StockDatabase): StockRepository {
+    override suspend fun insertStock(stockEntity: Stock
