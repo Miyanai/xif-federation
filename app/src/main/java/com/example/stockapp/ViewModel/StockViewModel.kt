914ddfc9fed3b86ebce0606abd00f717afb8c813
Miyanai <106360905+Miyanai@users.noreply.github.com>
2021-06-12 09:53:18 +0000

    Updated error messages

--- app/src/main/java/com/example/stockapp/ViewModel/StockViewModel.kt
+++ app/src/main/java/com/example/stockapp/ViewModel/StockViewModel.kt
@@ -2,4 +2,6 @@ package com.example.stockapp.ViewModel
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableSt
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.MutableLiveData
+import
