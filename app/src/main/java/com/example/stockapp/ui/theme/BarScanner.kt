633af084d8a63286a9eeff68a65275cef82a4e7d
Miyanai <106360905+Miyanai@users.noreply.github.com>
2021-11-08 12:59:18 +0000

    i18n

--- app/src/main/java/com/example/stockapp/ui/theme/BarScanner.kt
+++ app/src/main/java/com/example/stockapp/ui/theme/BarScanner.kt
@@ -18,3 +18,58 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.navArgument
+import com.example.stockapp.BottomScreen
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
+
+
+class BarScanner(context: Context, navController: NavController): ImageAnalysis.Analyzer {
+
+    var context: Context? = null
+    var navController: NavController? = null
+
+
+    init {
+
+        this.context = context
+        this.navController = navController
+
+    }
+
+    val options = BarcodeScannerOptions.Builder()
+        .setBarcodeFormats(
+            Barcode.FORMAT_QR_CODE,
+            Barcode.FORMAT_AZTEC,
+            Barcode.FORMAT_CODE_128,
+            Barcode.FORMAT_CODE_93,
+            Barcode.FORMAT_CODABAR,
+            Barcode.FORMAT_EAN_13,
+            Barcode.FORMAT_EAN_8,
+            Barcode.FORMAT_ITF,
+            Barcode.FORMAT_UPC_A,
+            Barcode.FORMAT_QR_CODE,
+            Barcode.FORMAT_DATA_MATRIX
+
+        )
+        .build()
+
+      private val scanner =BarcodeScanning.getClient(options)
+
+
+
+    @SuppressLint("UnsafeOptInUsageError")
+    override fun analyze(imageproxy: ImageProxy) {
+
+
+
+        val mediaImage = imageproxy.image
+        if(mediaImage != null){
+           val image = InputImage.fromMediaImage(
+               mediaImage, imageproxy.imageInfo.rotationDegrees
+           )
+
+            scanner.process(image).addOnSuccessListener{
+                barcodes ->
