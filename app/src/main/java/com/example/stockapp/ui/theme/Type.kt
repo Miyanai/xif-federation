c767e68eb2a2f6fa0d22a73f9018278917c3dc97
Miyanai <106360905+Miyanai@users.noreply.github.com>
2021-11-11 18:39:43 +0000

    blank lines

--- app/src/main/java/com/example/stockapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/stockapp/ui/theme/Type.kt
@@ -1,4 +1,5 @@
 package com.example.stockapp.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamil
