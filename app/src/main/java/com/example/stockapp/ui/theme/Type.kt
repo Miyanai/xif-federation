e0bc3055168759a0f48fa4565033c9e3ec550624
Miyanai <106360905+Miyanai@users.noreply.github.com>
2021-11-12 15:31:12 +0000

    Улучшено описание репозитория

--- app/src/main/java/com/example/stockapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/stockapp/ui/theme/Type.kt
@@ -2,4 +2,5 @@ package com.example.stockapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamil
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWe
