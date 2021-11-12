13e569599cb9c50d493a4873e9f65a9a3347f6fb
Miyanai <106360905+Miyanai@users.noreply.github.com>
2021-11-12 16:38:42 +0000

    Ignore tmp

--- app/src/main/java/com/example/stockapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/stockapp/ui/theme/Type.kt
@@ -3,4 +3,7 @@ package com.example.stockapp.ui.theme
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWe
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Mate
