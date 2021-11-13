a1d530d098186bc2d789cf7ead61ea640399d486
Miyanai <106360905+Miyanai@users.noreply.github.com>
2021-11-13 09:40:42 +0000

    USB: cdc-acm: fix runtime PM for control messages

--- app/src/main/java/com/example/stockapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/stockapp/ui/theme/Type.kt
@@ -7,4 +7,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
-val Typography = Typog
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontF
