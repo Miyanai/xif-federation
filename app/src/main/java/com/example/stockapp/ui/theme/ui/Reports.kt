e5d882b083dc28e5504653313a5d40faf3c00b7b
Miyanai <106360905+Miyanai@users.noreply.github.com>
2021-11-19 08:44:56 +0000

    fix many bugs

--- app/src/main/java/com/example/stockapp/ui/theme/ui/Reports.kt
+++ app/src/main/java/com/example/stockapp/ui/theme/ui/Reports.kt
@@ -3,4 +3,5 @@ package com.example.stockapp.ui.theme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
-impor
+import androidx.compose.runtime.Composable
+import androidx.
