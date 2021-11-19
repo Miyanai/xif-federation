b540cb205d14ccd6c8674a2b1b96790628f5e85c
Miyanai <106360905+Miyanai@users.noreply.github.com>
2021-11-19 05:52:17 +0000

    Gem update

--- app/src/main/java/com/example/stockapp/ui/theme/ui/Reports.kt
+++ app/src/main/java/com/example/stockapp/ui/theme/ui/Reports.kt
@@ -1,4 +1,6 @@
 package com.example.stockapp.ui.theme
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+impor
